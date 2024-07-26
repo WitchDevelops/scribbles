6b86ef082fc555463449a8c213314b34761af175
Dominika Wojewska <d.n.wojewska@gmail.com>
2024-07-26 20:20:59 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { Metadata } from "next";
+
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Scribbles",
+  description: "Your go-to collaborative text editor",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
